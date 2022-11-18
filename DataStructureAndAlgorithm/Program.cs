7152c76b744009953f81f2592d7d2365967903f0
nikitabolkar123 <bolkarnikita2001@gmail.com>
2022-11-18 08:20:14 -0800

    Added BS solution

---
+++ DataStructureAndAlgorithm/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DataStructureAndAlgorithm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Select Programs\n 1.Binary search");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        BinarySearch binarysearch = new BinarySearch();
+                        binarysearch.Search(@"C:\Users\hp\Desktop\CsharpCode\DataStructureAndAlgirithm\DataStructureAndAlgorithmProblems\DataStructureAndAlgorithm\Word.txt");
+                        break;
+                }
+            }
+        }
+    }
+}
